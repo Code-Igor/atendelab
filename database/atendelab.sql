3df7725740ba59b4ec8d376735de5156a24a01ae
Igor <igordossantoslopes73@gmail.com>
2026-06-09 15:37:26 -0300

    adicionando o perfil 'aluno' no sql.

--- database/atendelab.sql
+++ database/atendelab.sql
@@ -11,13 +11,13 @@ CREATE TABLE usuarios (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-    perfil ENUM('admin', 'atendente') DEFAULT 'atendente',
+    perfil ENUM('admin', 'aluno', 'atendente') DEFAULT 'atendente',
     status ENUM('ativo', 'inativo') DEFAULT 'ativo',
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE pessoas ( 
-    id INT AUTO_INCREMENT PRIMARY KEY, 
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL, 
     perfil ENUM('aluno', 'pessoa_atendida') DEFAULT 'pessoa_atendida',
     email VARCHAR(100) UNIQUE,
